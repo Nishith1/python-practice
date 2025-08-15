69ea3c38fca58b55f556f3f4df943ec80c75e9e9
Nishith <imnishith@gmail.com>
2025-08-15 07:35:03 +0530

    basic python syntaxes

--- main.py
+++ main.py
@@ -1,1 +1,49 @@
 print("Hello World")
+
+# name = "Nishith"
+# age = 36
+# height = 5.8
+# is_developer = True
+#
+# print("Name : ",name)
+# print("Age", age)
+# print("height", height)
+# print("is Developer", is_developer)
+#
+#
+# print(type(name))
+# print(type(age))
+# print(type(height))
+# print(type(is_developer))
+
+# name = input("Enter your name: ")
+# print("Hello,",name)
+#
+# age = int(input("Enter your age "))
+# print("In 5 years you will be =", age+5)
+
+# name = "Nishith"
+# age = 36
+# # print("Age ",age, "Name",name)
+#
+# print(f"{name},in 5 years you will be {age+5}, years old")
+
+
+# name = input("What is your name?")
+# print(f"Welcome, {name}")
+
+# birth_year = int(input("What is your birth Year"))
+# current_year = 2025
+#
+# age = current_year - birth_year
+# print(f"You are {age} years old.")
+
+# weight = float(input("Enter your weight in Kg: "))
+# height = float(input("Enter your height in meters: "))
+# bmi = weight / (height **2)
+# print(f"Your BMI is : {bmi: .2f}")
+
+
+
+
+
